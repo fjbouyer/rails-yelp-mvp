d454e67c1d4eb53cf20e6a99bed86a9b60919796
Francois Joseph Bouyer <fjbouyer@gmail.com>
2017-07-12 11:58:13 +0200

    model Restaurant + Review

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  validates :name, uniqueness: true, presence:true
+  validates :address, uniqueness: true, presence:true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+end
